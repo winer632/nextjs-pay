f5f6463d083c836330427f9d708d86b419f7bef9
wangxinxin <wangxinxin@sensetime.com>
2023-06-03 06:28:59 +0800

    currency type cny currency amount 1

--- pages/api/create-payment-intent.js
+++ pages/api/create-payment-intent.js
@@ -5,7 +5,7 @@ const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 1400;
+  return 1;
 };
 
 export default async function handler(req, res) {
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "eur",
+    currency: "cny",
     automatic_payment_methods: {
       enabled: true,
     },
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function App() {
     fetch("/api/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+      body: JSON.stringify({ items: [{ id: "per_month" }] }),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
